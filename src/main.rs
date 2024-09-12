e09cdc9b5d69581c556d3de8a5069456071c0060
maneeshjohn <maneeshjohno@gmail.com>
2024-09-12 22:24:23 +0530

    added shuffle and deal methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use rand::{thread_rng, seq::SliceRandom};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Deck {
+        let suits = ["Hearts", "Clubs", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        return Deck{ cards };
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        return self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, size: usize) -> Vec<String> {
+        return self.cards.split_off(self.cards.len() - size);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut deck = Deck::new();
+    deck.shuffle();
+    let hand = deck.deal(4);
+    print!("Hand: {:?}", hand);
+    print!("Deck: {:?}", deck);
 }
